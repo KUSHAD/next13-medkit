8c43bd6c81b92c2db8b0ea5fbe643aea95167749
AMITAVA CHAKRABORTY <Amitava.Chakraborty@kyndryl.com>
2023-07-07 17:48:20 +0530

    added vercel analytics to my project

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
 import { Nunito } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
 const nunito = Nunito({ subsets: ['latin'] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
 					<div className='w-full'>{children}</div>
 					<Toaster />
 				</div>
+				<Analytics />
 			</body>
 		</html>
 	);
